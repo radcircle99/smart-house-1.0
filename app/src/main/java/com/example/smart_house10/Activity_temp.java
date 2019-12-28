fe877af180cc7e7b77f0cf24b0ef732b81779928
Joris da MATHA <radcirclemath@gmail.com>
2019-12-28 21:43:21 +0100

    fourth commit

---
+++ app/src/main/java/com/example/smart_house10/Activity_temp.java
@@ -0,0 +1,14 @@
+package com.example.smart_house10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_temp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp);
+    }
+}
