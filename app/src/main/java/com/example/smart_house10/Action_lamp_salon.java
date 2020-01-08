9f6d733c9cf108003d930372a840a708f5a54c3e
Joris da MATHA <radcirclemath@gmail.com>
2020-01-08 22:00:02 +0100

    update6

---
+++ app/src/main/java/com/example/smart_house10/Action_lamp_salon.java
@@ -0,0 +1,14 @@
+package com.example.smart_house10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Action_lamp_salon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action_lamp_salon);
+    }
+}
